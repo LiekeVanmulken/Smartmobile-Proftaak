32ceb8c593c28718d25385adbadffa36597ec123
DevNebulae <gamerkeeperson+github@gmail.com>
2016-06-10 13:11:29 +0200

    Basic navigation and app layout is done

--- app/src/main/java/com/wouterv/quantifiedstudents/MainActivity.java
+++ app/src/main/java/com/wouterv/quantifiedstudents/MainActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
@@ -16,44 +18,28 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    PieChart pieChart;
+    private ImageView currentyNyxEmoji;
+    private TextView currentStatusText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        makePieChart();
-    }
-    public void makePieChart(){
-        pieChart = (PieChart) findViewById(R.id.scoreChart);
-
-        ArrayList<Entry> entries = new ArrayList<>();
-        float percentage = 82f; //calculate this later
-
-        entries.add(new Entry(percentage, 0));
-        entries.add(new Entry(100-percentage, 1));
-
-        PieDataSet dataset = new PieDataSet(entries,"");
-        dataset.setColors(new int[]{getResources().getColor(R.color.Orange),0});
-
-
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("");
-        labels.add("");
-        PieData data = new PieData(labels, dataset);
-        pieChart.setData(data);
-        pieChart.setCenterText("90%");
-        pieChart.setCenterTextSize(50);
-        pieChart.animateY(1000);
-        pieChart.getData().setValueTextColor(Color.TRANSPARENT);
-        pieChart.setDescription("");    // Hide the description
-        pieChart.getLegend().setEnabled(false);   // Hide the legend
-        pieChart.setClickable(false);
     }
 
-    public void goToActivityChart(View view) {
-        Intent myIntent = new Intent(this, ChartActivity.class);
-        startActivity(myIntent);
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        this.currentyNyxEmoji = (ImageView) this.findViewById(R.id.currentNyxEmoji);
+        this.currentStatusText = (TextView) this.findViewById(R.id.currentStatusText);
+
+        this.currentyNyxEmoji.setImageResource(R.drawable.happy_512px);
+        this.currentStatusText.setText("Your Nyx is very happy! Keep it up!");
+    }
+
+    public void navigate(View view) {
+        this.startActivity(new Intent(this, OverviewActivity.class));
     }
 }
